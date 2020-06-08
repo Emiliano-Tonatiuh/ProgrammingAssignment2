6b730458cf09c43aa382a02ff6dad7fd248b9c6b
Emiliano-Tonatiuh <65556190+Emiliano-Tonatiuh@users.noreply.github.com>
2020-06-08 16:57:42 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+
 
 ## Write a short comment describing this function
+
+#Esta función calcula la matriz inversa de una matriz dada 
 
 makeCacheMatrix <- function(x = matrix()) {
   inversa <- NULL
   est <- function(y){ x <<- y 
   inversa <<- NULL}
   obt <- function() x
-  estInv <- function(solveMatrix) inversa <<- solveMatrix
-  obtInv <- function() inversa
+  estInv <- function(solveMatrix) inversa <<- solveMatrix  #Aquí se calcula la matriz inversa
+  obtInv <- function() inversa 
   list(estInv = estInv, obtInv = obtInv,obt = obt,est = est)
 }
 
 
-## Write a short comment describing this function
 
+#Esta función calcula la matriz inversa de la proveniente de la función de arriba
 cacheSolve <- function(x, ...) {
         ## REGRESA LA MATRIZ INVERSA DE  'X'
   inversa <- x$obtInv()
